80dbdbbbd9e76a51f36aff76177e5f31eab6178d
Joe Bugajski <yogurtmystery@gmail.com>
2019-07-11 11:26:09 -0500

    args

--- Chapter 7/args-copy.py
+++ Chapter 7/args-copy.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# Functions allow variable-length argument lists
 def main():
     kitten('meow', 'grrr', 'purr')
-
-def kitten(*args):
-    if len(args):
+# We treat it as a sequence, actually a tuple
+def kitten(*args): # It's denoted as *args. args is the conventional name
+    if len(args): # If the length of args is greater than zero
         for s in args:
-            print(s)
-    else: print('Meow.')
+            print(s) # If len(args), print all the items in the tuple
+    else: print('Meow.') # otherwise print meow
 
 if __name__ == '__main__': main()
